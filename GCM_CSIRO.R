2aafa7bcc7b63c84521fe760360db2f75ff64982
MS Vanessa Neo <46217029@students.mq.edu.au>
2021-06-04 17:26:20 +1000

    stack netcdf data file

--- GCM_CSIRO.R
+++ GCM_CSIRO.R
@@ -3,7 +3,11 @@ library(rgdal)
 library(raster)
 library(ncdf4)
 
-mypath<-""#path to ncdf files
+#path to ncdf files
+mypath<-"D:/GCM_Data"
 
-csro_mod1_hist<-stack(paste(mypath,filename))
+csro_mod1_hist<- stack(paste(mypath,
+                            "tos_day_ACCESS1-0_historical_r3i1p1_19700101-19791231.nc", 
+                            sep = "/"), varname = "tos")
 
+csro_mod1_hist
