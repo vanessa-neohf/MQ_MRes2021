9b54dd8a7be29a65f6508e33ce21eb7b4120173d
MS Vanessa Neo <46217029@students.mq.edu.au>
2021-06-22 13:02:19 +1000

    removed duplicates and exported CSV

--- Loggers_Avg_Daily_SST.R
+++ Loggers_Avg_Daily_SST.R
@@ -36,59 +36,67 @@ Log_NTHFISHFL1 <- read_csv("NTHFISHFL1.csv",
 tbl_100TH <- Log_100THSITE %>% 
   mutate(date = format(as.Date(time))) %>% 
   group_by(date) %>% 
-  summarize(mean(qc_val, na.rm = TRUE))
+  summarize(mean_SST = mean(qc_val, na.rm = TRUE))
 
 tbl_BUN_BR <- Log_BUNDEGI_BR %>% 
   mutate(date = format(as.Date(time))) %>% 
   group_by(date) %>% 
-  summarize(mean(qc_val, na.rm = TRUE))
+  summarize(mean_SST = mean(qc_val, na.rm = TRUE))
 
 tbl_BUNDFL1 <- Log_BUNDFL1 %>% 
   mutate(date = format(as.Date(time))) %>% 
   group_by(date) %>% 
-  summarize(mean(qc_val, na.rm = TRUE))
+  summarize(mean_SST = mean(qc_val, na.rm = TRUE))
 
 tbl_NTHFISHFL1 <- Log_NTHFISHFL1 %>% 
   mutate(date = format(as.Date(time))) %>% 
   group_by(date) %>% 
-  summarize(mean(qc_val, na.rm = TRUE))
+  summarize(mean_SST = mean(qc_val, na.rm = TRUE))
 
 tbl_SCOTTSS1 <- Log_SCOTTSS1 %>% 
   mutate(date = format(as.Date(time))) %>% 
   group_by(date) %>% 
-  summarize(mean(qc_val, na.rm = TRUE))
+  summarize(mean_SST = mean(qc_val, na.rm = TRUE))
 
 tbl_TANDFL1 <- Log_TANDFL1 %>% 
   mutate(date = format(as.Date(time))) %>% 
   group_by(date) %>% 
-  summarize(mean(qc_val, na.rm = TRUE))
+  summarize(mean_SST = mean(qc_val, na.rm = TRUE))
 
 tbl_TANTABIDDI_SL1 <- Log_TANTABIDDI_SL1 %>% 
   mutate(date = format(as.Date(time))) %>% 
   group_by(date) %>% 
-  summarize(mean(qc_val, na.rm = TRUE))
+  summarize(mean_SST = mean(qc_val, na.rm = TRUE))
 
 #add date column to logger files (for joining with summary table)
+#remove duplicate date columns with distinct()
 Log_100THSITE <- Log_100THSITE %>% 
-  mutate(date = format(as.Date(time)))
+  mutate(date = format(as.Date(time))) %>% 
+  distinct(date, .keep_all = TRUE)
 
 Log_BUNDEGI_BR <- Log_BUNDEGI_BR %>% 
-  mutate(date = format(as.Date(time)))
+  mutate(date = format(as.Date(time))) %>% 
+  distinct(date, .keep_all = TRUE)
 
 Log_BUNDFL1 <- Log_BUNDFL1 %>% 
-  mutate(date = format(as.Date(time)))
+  mutate(date = format(as.Date(time))) %>% 
+  distinct(date, .keep_all = TRUE)
 
 Log_NTHFISHFL1 <- Log_NTHFISHFL1 %>% 
-  mutate(date = format(as.Date(time)))
+  mutate(date = format(as.Date(time))) %>% 
+  distinct(date, .keep_all = TRUE)
 
 Log_SCOTTSS1 <- Log_SCOTTSS1 %>% 
-  mutate(date = format(as.Date(time)))
+  mutate(date = format(as.Date(time))) %>% 
+  distinct(date, .keep_all = TRUE)
 
 Log_TANDFL1 <- Log_TANDFL1 %>% 
-  mutate(date = format(as.Date(time)))
+  mutate(date = format(as.Date(time))) %>% 
+  distinct(date, .keep_all = TRUE)
 
 Log_TANTABIDDI_SL1 <- Log_TANTABIDDI_SL1 %>% 
-  mutate(date = format(as.Date(time)))
+  mutate(date = format(as.Date(time))) %>% 
+  distinct(date, .keep_all = TRUE)
 
 #join SST values in summary table with logger file
 Log_100THSITE <- left_join(Log_100THSITE, tbl_100TH, by = "date")
@@ -98,3 +106,12 @@ Log_NTHFISHFL1 <- left_join(Log_NTHFISHFL1, tbl_NTHFISHFL1, by = "date")
 Log_SCOTTSS1 <- left_join(Log_SCOTTSS1, tbl_SCOTTSS1, by = "date")
 Log_TANDFL1 <- left_join(Log_TANDFL1, tbl_TANDFL1, by = "date")
 Log_TANTABIDDI_SL1 <- left_join(Log_TANTABIDDI_SL1, tbl_TANTABIDDI_SL1, by = "date")
+
+#export csv
+write_csv(Log_100THSITE, "D:/Logger_Data/Avg_Daily_SST_Loggers/Logger_Avg_Daily_SST_100THSITE.csv")
+write_csv(Log_BUNDEGI_BR, "D:/Logger_Data/Avg_Daily_SST_Loggers/Logger_Avg_Daily_SST_BUNDEGI_BR.csv")
+write_csv(Log_BUNDFL1, "D:/Logger_Data/Avg_Daily_SST_Loggers/Logger_Avg_Daily_SST_BUNDFL1.csv")
+write_csv(Log_NTHFISHFL1, "D:/Logger_Data/Avg_Daily_SST_Loggers/Logger_Avg_Daily_SST_NTHFISHFL1.csv")
+write_csv(Log_SCOTTSS1, "D:/Logger_Data/Avg_Daily_SST_Loggers/Logger_Avg_Daily_SST_SCOTTSS1.csv")
+write_csv(Log_TANDFL1, "D:/Logger_Data/Avg_Daily_SST_Loggers/Logger_Avg_Daily_SST_TANDFL1.csv")
+write_csv(Log_TANTABIDDI_SL1, "D:/Logger_Data/Avg_Daily_SST_Loggers/Logger_Avg_Daily_SST_TANTABIDDI_SL1.csv")
